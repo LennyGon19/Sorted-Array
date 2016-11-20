399849646dd3c512cdaf8e72d618e6d1877c0ee6
Lenny Gonzalez <lenny.gonzaleznyc@gmail.com>
2016-11-19 21:35:19 -0500

    Add files via upload

---
+++ Array Sorting - Merge Sort/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main
+{
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        Integer[] a = new Integer[size];
+        System.out.println("Enter all the integers you want to store: " );
+
+        for(int i = 0; i < a.length; i++)
+        {
+            a[i] = input.nextInt();
+        }
+        mergeSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void mergeSort(Comparable [ ] a)
+    {
+        Comparable[] tmp = new Comparable[a.length];
+        mergeSort(a, tmp,  0,  a.length - 1);
+    }
+
+
+    private static void mergeSort(Comparable [ ] a, Comparable [ ] tmp, int left, int right)
+    {
+        if( left < right )
+        {
+            int center = (left + right) / 2;
+            mergeSort(a, tmp, left, center);
+            mergeSort(a, tmp, center + 1, right);
+            merge(a, tmp, left, center + 1, right);
+        }
+    }
+
+
+    private static void merge(Comparable[ ] a, Comparable[ ] tmp, int left, int right, int rightEnd )
+    {
+        int leftEnd = right - 1;
+        int k = left;
+        int num = rightEnd - left + 1;
+
+        while(left <= leftEnd && right <= rightEnd)
+            if(a[left].compareTo(a[right]) <= 0)
+                tmp[k++] = a[left++];
+            else
+                tmp[k++] = a[right++];
+
+        while(left <= leftEnd)    // Copy rest of first half
+            tmp[k++] = a[left++];
+
+        while(right <= rightEnd)  // Copy rest of right half
+            tmp[k++] = a[right++];
+
+        // Copy tmp back
+        for(int i = 0; i < num; i++, rightEnd--)
+            a[rightEnd] = tmp[rightEnd];
+    }
+}
